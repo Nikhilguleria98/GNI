c385dd7878aba105303a3fe00edb4006caeaf05e
daman-25 <kaurdamanpreet2511@gmail.com>
2026-09-10 15:28:56 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-red-200 text-white text-center'>
+      welcome to gni
+    </div>
+  )
+}
+
+export default App
